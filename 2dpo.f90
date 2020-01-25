32470dcd61befcc265fbc33e24b11eaee643b1b3
atsushino <atsushino@users.noreply.github.com>
2020-01-25 23:06:30 +0900

    grid gen&init

---
+++ 2dpo.f90
@@ -0,0 +1,29 @@
+    program po2d
+
+
+
+    implicit none
+    integer, allocatable :: psi(:,:), omg(:,:) ,tmp(:,:)
+    integer                 i, j, k
+    integer                 Nmesh, Stop_itr
+    real                    Re, dt, eps
+
+
+    write(*,*) 'input meshsize'
+    read(*,*) Nmesh
+
+    allocate(psi(Nmesh,Nmesh))
+    allocate(omg(Nmesh,Nmesh))
+    allocate(tmp(Nmesh,Nmesh))
+
+
+    do i = 1, Nmesh
+      psi(:,:)=0.0
+      omg(:,:)=0.0
+      tmp(:,:)=0.0
+    end do
+
+    write(*,*)'Mesh size=',Nmesh,'*',Nmesh,'. all grid initialized.'
+    write(*,*) psi, omg, tmp
+    stop
+    end
