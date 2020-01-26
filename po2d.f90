52621cfe3bf8e6165c2b4bbaf1dc986bac91251e
atsushino <atsushino@users.noreply.github.com>
2020-01-26 13:08:21 +0900

    fix calc omg (del do)

--- po2d.f90
+++ po2d.f90
@@ -5,7 +5,7 @@
     implicit none
     integer, allocatable :: psi(:,:), omg(:,:) ,tmp(:,:)
     integer                 i, j, k, l
-    integer                 Nmesh, Stop_itr, GZ_itr
+    integer                 Nmesh, Stop_itr
     real                    Re, dt, eps, const
     real                    h, rhsp, rhso, bb, err
 
@@ -29,7 +29,7 @@
     write(*,*)'============== Grid generation: Done ============='
 
 
-    Stop_itr=100
+    Stop_itr=1000
 
     !MAIN LOOP
     do l = 1, Stop_itr
@@ -39,29 +39,28 @@
       !left/right
       do j = 1, Nmesh
 
-        omg(1,j)      = -2.0*psi(2      ,j) /h**2.0
-        omg(Nmesh,j)  = -2.0*psi(Nmesh-1,j) /h**2.0
+        omg(1,j)      = -2.0*psi(2      ,j) /(h**2.0)
+        omg(Nmesh,j)  = -2.0*psi(Nmesh-1,j) /(h**2.0)
 
       end do
 
       !bottom/top
       do i = 1, Nmesh
 
-        omg(i,1)      = -2.0*psi(i,2)      /h**2.0
-        omg(i,Nmesh)  = -2.0*(psi(i,Nmesh-1)+h)/h**2.0
+        omg(i,1)      = -2.0*psi(i ,         2)/(h**2.0)
+        omg(i,Nmesh)  = -2.0*(psi(i,Nmesh-1)+h)/(h**2.0)
 
       end do
-
+      !write(*,*)omg !!!!!!!!!
       !========END boundary condition for cuvic cavity
 
 
 
       !calc psi
-      GZ_itr = 100
-      const = 1.0
-      Re=0.20
-      dt= 0.0025
-      eps=0.00001
+      const = 1.0 !!!!!!!
+      Re=40.0 !!!!!!!!
+      dt= 0.01 !!!!!!!!!!!!!
+      eps=0.00001 !!!!!!!!!!!!!
       do while (err.gt.eps)
         do j = 2, Nmesh-1
           do i = 2, Nmesh-1
@@ -72,7 +71,7 @@
                   +omg(i,j)*h**2.0/4.0-psi(i,j)
 
             psi(i,j) = psi(i,j)+const*rhsp
-
+            !write(*,*)psi!!!!!!!!!!
           end do
         end do
 
@@ -89,30 +88,28 @@
 
 
       !calc omg
-      do k = 1, GZ_itr
-        do j = 2, Nmesh-1
-          do i = 2, Nmesh-1
+      do j = 2, Nmesh-1
+        do i = 2, Nmesh-1
 
-            tmp(i,j) = omg(i,j)
+          tmp(i,j) = omg(i,j)
 
-            rhso = ((psi(i+1,j)-psi(i-1,j))*(omg(i,j+1)-omg(i,j-1)) &
-                  -(psi(i,j+1)-psi(i,j-1))*(omg(i+1,j)-omg(i-1,j)))/4.0 &
-                  +(omg(i+1,j)-omg(i-1,j)+omg(i,j+1)-omg(i,j-1) &
-                  -4.0*omg(i,j))/Re
+          rhso = ((psi(i+1,j)-psi(i-1,j))*(omg(i,j+1)-omg(i,j-1)) &
+                -(psi(i,j+1)-psi(i,j-1))*(omg(i+1,j)-omg(i-1,j)))/4.0 &
+                +(omg(i+1,j)-omg(i-1,j)+omg(i,j+1)-omg(i,j-1) &
+                -4.0*omg(i,j))/Re
 
-            omg(i,j) = omg(i,j)+dt*rhso/h**2
-
-          end do
+          omg(i,j) = omg(i,j)+dt*rhso/(h**2)
+          !write(*,*) omg!!!!!!!
         end do
       end do
 
 
-
-      write(*,*)psi,omg
-      write(*,*)'==========================',l
+      write(*,*)'iteration=',l
 
       !close MAIN LOOP
     end do
-
+      write(*,*)psi
+      write(*,*)
+      write(*,*)omg
     stop
     end
